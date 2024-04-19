1bf71e7ab07f39785341bd907d6fb2c73081e167
Axl Armando Castrejón Ocampo <redotola117@gmail.com>
2024-04-19 12:20:13 -0600

    Actualización Formulario Registro Usuario

--- src/html/assets/js/crear_usuario.js
+++ src/html/assets/js/crear_usuario.js
@@ -1,27 +1,41 @@
 /* eslint-disable prettier/prettier */
-// eslint-disable-next-line prettier/prettier
 document.addEventListener("DOMContentLoaded", function() {
   // Obtener el formulario
   let form = document.getElementById("commentForm1");
-  
+
   // Agregar un evento de escucha al envío del formulario
   form.addEventListener("submit", function(event) {
       event.preventDefault(); // Prevenir el envío del formulario
-  
+
       // Obtener los datos del formulario
       let formData = new FormData(form);
-  
-      // Construir el mensaje de alerta con los datos del formulario
-      let message = "Información Personal:\n";
+      let allFieldsFilled = true; // Variable para rastrear si todos los campos están llenos
+
+      // Verificar si algún campo está vacío
       formData.forEach(function(value, key) {
-          message += key.charAt(0).toUpperCase() + key.slice(1) + ": " + value + "\n";
+          if (value.trim() === "") { // Si el valor del campo está vacío después de eliminar espacios en blanco
+              allFieldsFilled = false;
+              alert("Por favor, completa todos los campos.");
+              return; // Salir del bucle forEach tan pronto como se encuentre un campo vacío
+          }
       });
-      
 
-      // Mostrar la alerta con los datos del formulario
-      console.log(message);
+      // Si todos los campos están llenos, continuar con el envío del formulario
+      if (allFieldsFilled) {
+          // Construir el mensaje de alerta con los datos del formulario
+          let message = "Información Personal:\n";
+          formData.forEach(function(value, key) {
+              message += key.charAt(0).toUpperCase() + key.slice(1) + ": " + value + "\n";
+          });
 
-      window.location.href = "../../HTML/index.html";
+          // Mostrar la alerta con los datos del formulario
+          console.log(message);
+          alert(message);
+
+          // Redirigir al usuario
+          window.location.href = "../../HTML/index.html";
+        }
   });
 });
+
 
