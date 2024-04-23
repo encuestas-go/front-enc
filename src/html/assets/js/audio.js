fb474b03a7646d54932b1084a375e5f49cdcc514
Axl Armando Castrejón Ocampo <redotola117@gmail.com>
2024-04-23 11:05:51 -0600

    Cambios Correctivos

--- src/html/assets/js/audio.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable prettier/prettier */
-// Obtener el elemento de audio
-var audio = document.getElementById("audio");
-
-// Reproducir el audio en bucle
-audio.play();
--- src/html/assets/js/encuesta_economica.js
+++ src/html/assets/js/encuesta_economica.js
@@ -15,9 +15,18 @@ document.addEventListener("DOMContentLoaded", function() {
         formData.forEach(function(value, key) {
             if (value.trim() === "") { // Si el valor del campo está vacío después de eliminar espacios en blanco
                 allFieldsFilled = false;
-                alert("El campo: " + key + " no ha sido completado, porfavor completa todos los campos.");
+                alert("El campo: " + key + " no ha sido completado, por favor completa todos los campos.");
                 return; // Salir del bucle forEach tan pronto como se encuentre un campo vacío
             }
+        });
+
+        // Obtener los valores de los checkboxes
+        let prestacionesValues = [];
+        let checkboxes = document.querySelectorAll('.select-input.birth-date-input.form-group input[type="checkbox"]:checked');
+        checkboxes.forEach(function(checkbox) {
+            let id = checkbox.id;
+            let label = checkbox.nextElementSibling.textContent;
+            prestacionesValues.push({ id: id, label: label });
         });
   
         // Si todos los campos están llenos, continuar con el envío del formulario
@@ -26,7 +35,12 @@ document.addEventListener("DOMContentLoaded", function() {
             let message = "Información Personal:\n";
             formData.forEach(function(value, key) {
                 message += key.charAt(0).toUpperCase() + key.slice(1) + ": " + value + "\n";
-                console.log(value);
+            });
+
+            // Agregar los valores de los checkboxes al mensaje de alerta
+            message += "\nTipos de Prestaciones Seleccionados:\n";
+            prestacionesValues.forEach(function(option) {
+                message += option.label.trim() + "\n";
             });
   
             // Mostrar la alerta con los datos del formulario
@@ -37,7 +51,4 @@ document.addEventListener("DOMContentLoaded", function() {
             window.location.href = "../../HTML/index.html";
           }
     });
-  });
-  
-  
-  
+});
