7dddd79ca3150e294128254a48ee76cab4da4f8e
Christian Hernandez Najera <chrismatla68@gmail.com>
2024-06-05 16:14:40 -0600

    report fix

--- src/Anfra_v2.1_main/login-register-1/assets/js/utils.js
+++ src/Anfra_v2.1_main/login-register-1/assets/js/utils.js
@@ -73,3 +73,11 @@ function showDeleteButton() {
 function hideDeleteButton() {
     document.getElementById("button-delete").style.display = "none";
 }
+
+function formatDate(date) {
+    const d = new Date(date);
+    const year = d.getFullYear();
+    const month = ('0' + (d.getMonth() + 1)).slice(-2);
+    const day = ('0' + d.getDate()).slice(-2);
+    return `${year}-${month}-${day}`;
+}
